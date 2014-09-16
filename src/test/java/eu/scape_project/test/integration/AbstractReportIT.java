0bc785209947b347d57ea4f2f37c9d8a38d63f45
fasseg <frank.asseg@objecthunter.net>
2014-09-16 10:08:07 +0200

    created fcrepo test instance for applying the report api tests

---
+++ src/test/java/eu/scape_project/test/integration/AbstractReportIT.java
@@ -0,0 +1,10 @@
+package eu.scape_project.test.integration;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/test-container.xml")
+public abstract class AbstractReportIT {
+}
---
+++ src/test/java/eu/scape_project/test/integration/ReportIT.java
@@ -0,0 +1,10 @@
+package eu.scape_project.test.integration;
+
+import org.junit.Test;
+
+public class ReportIT extends AbstractReportIT {
+    @Test
+    public void testGetMetadataFormats() throws Exception{
+
+    }
+}
